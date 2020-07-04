ec53e289a29412246296c2b3b9b89f08e22ec634
Baecode <byp93@naver.com>
2020-07-04 13:12:48 +0900

    expect 추가.

---
+++ testLib/ExpectFunction.dart
@@ -0,0 +1,20 @@
+import 'package:test/test.dart';
+
+main(List<String> args) {
+  test("number Test",(){
+    expect(10, equals(10));
+  });
+  test("String Test",(){
+    expect("ABC", equals("ABC"));
+  });
+  test("List Test",(){
+    List<String> list = ["AB","CD","EF"];
+    expect(list, equals(["AB","CD","EF"]));
+  });
+
+
+  test("String Test Fail",(){
+    expect("ABC", equals("ABCD"));
+  });
+  
+}
