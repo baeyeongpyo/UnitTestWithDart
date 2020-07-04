a9f235dc8581d86e2fb767761095997fc612f662
Baecode <byp93@naver.com>
2020-07-04 11:50:27 +0900

    test lib 추가 및 test 함수 실행 

---
+++ testLib/TestFunction.dart
@@ -0,0 +1,22 @@
+import 'package:test/test.dart';
+
+main(List<String> args) {
+  test("test 1",(){
+    print("test 1 print");
+  });
+  test("test 2",(){
+    print("test 2 print");
+  });
+  test("test 3",(){
+    print("test 3 print");
+  });
+  test("test 4",(){
+    print("test 4 print");
+  });
+  test("test 5",(){
+    print("test 5 print");
+  });
+  test("test 6",(){
+    print("test 6 print");
+  });
+}
