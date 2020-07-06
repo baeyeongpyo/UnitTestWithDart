4217a9563ff649beebff5ce5d476ec9758fe0844
Baecode <byp93@naver.com>
2020-07-06 23:19:28 +0900

    skip 추가.

---
+++ testLib/SkipFunction.dart
@@ -0,0 +1,14 @@
+import 'package:test/test.dart';
+
+main(List<String> args) {
+  
+  test("test 1", () {
+    expect(true, equals(false));
+  }, skip: "test 1 skip");
+  test("test 2", () {
+    expect(true, equals(true));
+  });
+  test("test 3", () {
+    expect(true, equals(true));
+  });
+}
