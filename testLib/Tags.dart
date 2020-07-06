6df073c48578d44c57fb43557a03198442bf336d
Baecode <byp93@naver.com>
2020-07-07 01:37:52 +0900

    tags or tags 추가

--- testLib/Tags.dart
+++ testLib/Tags.dart
@@ -2,13 +2,12 @@ import 'package:test/test.dart';
 
 @Tags(const ["chrom"])
 main() {
-  test("tag chrom", (){
-    
-  }, tags: "chrom");
+  test("tag chrom", () {}, tags: "chrom");
 
-  test("tag explor", (){
-    
-  }, tags: "explor");
+  test("tag explor", () {}, tags: "explor");
+
+  test("tag window", () {}, tags: "window");
 }
 
 // pub run test testLib/Tags.dart --exclude-tags "chrom"
+// pub run test testLib/Tags.dart --exclude-tags "(chrom || explor)"
