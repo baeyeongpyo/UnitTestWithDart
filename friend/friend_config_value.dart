0daf5b9a7f081dbfaf2f84b59af033d4cb763f16
Beacode <byp93@naver.com>
2020-07-09 14:20:55 +0900

    friend util class 추가.

---
+++ friend/friend_config_value.dart
@@ -0,0 +1,4 @@
+class FriendConfigVaule {
+  static const int AUTH_USER_MAX_FRIDED_COUNT = 50;
+  static const int AUTH_NOT_USER_MAX_FRIDED_COUNT = 3;
+}
--- friend/friend_util.dart
+++ friend/friend_util.dart
@@ -1,5 +1,23 @@
-class friendUtil{
+import 'data/friend_state_enum.dart';
+import 'data/friend_vo.dart';
+import 'friend_config_value.dart';
 
-  
+class friendUtil {
+  bool isfriendApplyCheck(FriendVO myInfo, FriendVO userInfo) {
+    int myFriendLimit = _friendLimitCount(myInfo);
+    int userFriendLimit = _friendLimitCount(userInfo);
+    return userInfo.firendState == FriendStateEnum.NOTHING ||
+        myFriendLimit <= myInfo.firendCount ||
+        userFriendLimit <= userInfo.firendCount;
+  }
 
+  int _friendLimitCount(FriendVO vo) {
+    int firendLimit;
+    if (vo.isAuth) {
+      firendLimit = FriendConfigVaule.AUTH_USER_MAX_FRIDED_COUNT;
+    } else {
+      firendLimit = FriendConfigVaule.AUTH_NOT_USER_MAX_FRIDED_COUNT;
+    }
+    return firendLimit;
+  }
 }
