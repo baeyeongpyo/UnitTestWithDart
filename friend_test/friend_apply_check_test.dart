d2cc30a7823c23113c564d66e7bbdf4e0738be32
Baecode <byp93@naver.com>
2020-07-12 16:32:14 +0900

    테스트 케이스 정리 및 오류 수정.

--- friend_test/friend_apply_check_test.dart
+++ friend_test/friend_apply_check_test.dart
@@ -5,48 +5,75 @@ import '../friend/data/friend_state_enum.dart';
 import '../friend/friend_util.dart';
 
 main() {
-  friendStateCheckToNOTHING();
-}
 
-friendStateCheckToNOTHING() {
   final FriendUtil friendUtil = FriendUtil();
-  group("사용자에게 친구 보내기 가능 여부 테스트 \\", () {
-    test("나 ( 친구 0 , 인증 x ), 상대 ( 친구 0 , 인증 x )", () {
-      final myInfo = new FriendVO(null, 0, FriendStateEnum.ME, false);
-      final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
-      expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), true);
-    });
-    test(
-        "나 ( 친구 ${FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT} , 인증 x ), 상대 ( 친구 0 , 인증 x )",
-        () {
-      final myInfo = new FriendVO(null, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT,
-          FriendStateEnum.ME, false);
-      final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
-      expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), false);
-    });
-    test(
-        "나 ( 친구 ${FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT} , 인증 o ), 상대 ( 친구 0 , 인증 x )",
-        () {
-      final myInfo = new FriendVO(
-          null, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, FriendStateEnum.ME, true);
-      final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
-      expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), true);
-    });
-    test(
-        "나 ( 친구 ${FriendUtil.FRIEND_MAX_COUNT} , 인증 x ), 상대 ( 친구 0 , 인증 x )",
-        () {
-      final myInfo = new FriendVO(null, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT,
-          FriendStateEnum.ME, false);
-      final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
-      expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), false);
-    });
-    test(
-        "나 ( 친구 ${FriendUtil.FRIEND_MAX_COUNT} , 인증 o ), 상대 ( 친구 0 , 인증 x )",
-        () {
-      final myInfo = new FriendVO(
-          null, FriendUtil.FRIEND_MAX_COUNT, FriendStateEnum.ME, true);
-      final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
-      expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), false);
-    });
+  group("사용자에게 친구 보내기 가능 여부 테스트", () {
+    friendTestHelper(FriendStateEnum.NOTHING, 0, false, 0, false, true);
+    friendTestHelper(FriendStateEnum.NOTHING, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, 0, false, false);
+    friendTestHelper(FriendStateEnum.NOTHING, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, 0, false, true);
+    friendTestHelper(FriendStateEnum.NOTHING, FriendUtil.FRIEND_MAX_COUNT, true, 0, false, false);
+
+    friendTestHelper(FriendStateEnum.NOTHING, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, false);
+    friendTestHelper(FriendStateEnum.NOTHING, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, false);
+    friendTestHelper(FriendStateEnum.NOTHING, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, true);
+
+    friendTestHelper(FriendStateEnum.NOTHING, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, false, false);
+    friendTestHelper(FriendStateEnum.NOTHING, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, true, false);
+    friendTestHelper(FriendStateEnum.NOTHING, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, true, false);
+
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, 0, false, 0, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, 0, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, 0, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, FriendUtil.FRIEND_MAX_COUNT, true, 0, false, false);
+
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, false);
+
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, true, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUESTED, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, true, false);
+    
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, 0, false, 0, false, false);
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, 0, false, false);
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, 0, false, false);
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, FriendUtil.FRIEND_MAX_COUNT, true, 0, false, false);
+
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, false);
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, false);
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, false);
+
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, false, false);
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, true, false);
+    friendTestHelper(FriendStateEnum.ALREADY_FRIENDS, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, true, false);
+    
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, 0, false, 0, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, 0, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, 0, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, FriendUtil.FRIEND_MAX_COUNT, true, 0, false, false);
+
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, false, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, true, false);
+
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, false, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, true, false);
+    friendTestHelper(FriendStateEnum.FRIENDS_REQUEST_RECEIVED, FriendUtil.FRIEND_MAX_COUNT, true, FriendUtil.FRIEND_MAX_COUNT, true, false);
   });
 }
+
+friendTestHelper(FriendStateEnum firendStatus, int myFriendCount, bool myAuth,
+    int userFriendCount, bool userAuth, bool check) {
+  FriendUtil friendUtil = FriendUtil();
+  String message =
+      "나 ( 친구 $myFriendCount, ${authCheck(myAuth)} ) , 상대 ( 친구 $userFriendCount ${authCheck(userAuth)} ) $firendStatus";
+  test(message, () {
+    final myInfo =
+        new FriendVO(null, myFriendCount, FriendStateEnum.ME, myAuth);
+    final userInfo =
+        new FriendVO(null, userFriendCount, firendStatus, userAuth);
+    expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), check);
+  });
+}
+
+String authCheck(bool isAuth) => "인증 ${isAuth ? "o" : "x"}";
