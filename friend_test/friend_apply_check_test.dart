d2afbfec0ea929b34aeeab859ddafefd54afac66
Baecode <byp93@naver.com>
2020-07-12 15:27:39 +0900

    테스트 케이스 분리 ( NOTHING )

--- friend_test/friend_apply_check_test.dart
+++ friend_test/friend_apply_check_test.dart
@@ -5,15 +5,46 @@ import '../friend/data/friend_state_enum.dart';
 import '../friend/friend_util.dart';
 
 main() {
+  friendStateCheckToNOTHING();
+}
+
+friendStateCheckToNOTHING() {
   final FriendUtil friendUtil = FriendUtil();
-  group("사용자에게 친구 보내기 가능 여부 테스트", () {
+  group("사용자에게 친구 보내기 가능 여부 테스트 \\", () {
     test("나 ( 친구 0 , 인증 x ), 상대 ( 친구 0 , 인증 x )", () {
       final myInfo = new FriendVO(null, 0, FriendStateEnum.ME, false);
       final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
       expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), true);
     });
-    test("나 ( 친구 ${FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT} , 인증 x ), 상대 ( 친구 0 , 인증 x )", () {
-      final myInfo = new FriendVO(null, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, FriendStateEnum.ME, false);
+    test(
+        "나 ( 친구 ${FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT} , 인증 x ), 상대 ( 친구 0 , 인증 x )",
+        () {
+      final myInfo = new FriendVO(null, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT,
+          FriendStateEnum.ME, false);
+      final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
+      expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), false);
+    });
+    test(
+        "나 ( 친구 ${FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT} , 인증 o ), 상대 ( 친구 0 , 인증 x )",
+        () {
+      final myInfo = new FriendVO(
+          null, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT, FriendStateEnum.ME, true);
+      final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
+      expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), true);
+    });
+    test(
+        "나 ( 친구 ${FriendUtil.FRIEND_MAX_COUNT} , 인증 x ), 상대 ( 친구 0 , 인증 x )",
+        () {
+      final myInfo = new FriendVO(null, FriendUtil.NOT_AUTH_FRIEND_MAX_COUNT,
+          FriendStateEnum.ME, false);
+      final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
+      expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), false);
+    });
+    test(
+        "나 ( 친구 ${FriendUtil.FRIEND_MAX_COUNT} , 인증 o ), 상대 ( 친구 0 , 인증 x )",
+        () {
+      final myInfo = new FriendVO(
+          null, FriendUtil.FRIEND_MAX_COUNT, FriendStateEnum.ME, true);
       final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
       expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), false);
     });
