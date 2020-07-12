e2768350857edc941262b28cfdc302be19ae6656
Baecode <byp93@naver.com>
2020-07-12 11:23:46 +0900

    친구 신청 확인 유틸 추가.

--- friend/friend_util.dart
+++ friend/friend_util.dart
@@ -1,23 +1,18 @@
 import 'data/friend_state_enum.dart';
 import 'data/friend_vo.dart';
-import 'friend_config_value.dart';
 
 class FriendUtil {
+  static const FRIEND_MAX_COUNT = 50;
+  static const NOT_AUTH_FRIEND_MAX_COUNT = 3;
+
   bool isfriendApplyCheck(FriendVO myInfo, FriendVO userInfo) {
-    int myFriendLimit = _friendLimitCount(myInfo);
-    int userFriendLimit = _friendLimitCount(userInfo);
-    return userInfo.firendState == FriendStateEnum.NOTHING ||
-        myFriendLimit <= myInfo.firendCount ||
-        userFriendLimit <= userInfo.firendCount;
+    bool stateCheck = userInfo.firendState == FriendStateEnum.NOTHING;
+    bool myFriendCheck = friendMAXCount(myInfo.isAuth) > myInfo.firendCount;
+    bool userFriendCheck =
+        friendMAXCount(userInfo.isAuth) > userInfo.firendCount;
+    return stateCheck || myFriendCheck || userFriendCheck;
   }
 
-  int _friendLimitCount(FriendVO vo) {
-    int firendLimit;
-    if (vo.isAuth) {
-      firendLimit = FriendConfigVaule.AUTH_USER_MAX_FRIDED_COUNT;
-    } else {
-      firendLimit = FriendConfigVaule.AUTH_NOT_USER_MAX_FRIDED_COUNT;
-    }
-    return firendLimit;
-  }
+  int friendMAXCount(bool isAuth) =>
+      isAuth ? FRIEND_MAX_COUNT : NOT_AUTH_FRIEND_MAX_COUNT;
 }
--- friend_test/friend_apply_check_test.dart
+++ friend_test/friend_apply_check_test.dart
@@ -7,7 +7,7 @@ import '../friend/friend_util.dart';
 main() {
   final FriendUtil friendUtil = FriendUtil();
   group("사용자에게 친구 보내기 가능 여부 테스트", () {
-    test("나 ( 친구 0 , 인증 ㅇ ), 상대 ( 친구 0 , 인증 ㅇ )", () {
+    test("나 ( 친구 0 , 인증 x ), 상대 ( 친구 0 , 인증 x )", () {
       final myInfo = new FriendVO(null, 0, FriendStateEnum.ME, false);
       final userInfo = new FriendVO(null, 0, FriendStateEnum.NOTHING, false);
       expect(friendUtil.isfriendApplyCheck(myInfo, userInfo), true);
